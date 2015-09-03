722ba463800822f94126380a77ea6a41f4f78e14
xujiaxuan <xujiaxuan@tp-link.net>
2015-09-03 14:59:26 +0800

    reflush

--- myself/ubuntu/mybin/makeCscopeDB.sh
+++ myself/ubuntu/mybin/makeCscopeDB.sh
@@ -2,5 +2,7 @@
 find `pwd` -name '*.h' >> cscope.files
 find `pwd` -name '*.c' >> cscope.files
 find `pwd` -name '*.cpp' >> cscope.files
+find `pwd` -name '*.java' >> cscope.files
+find `pwd` -name '*.xml' >> cscope.files
 cscope -Rbkq -i cscope.files
 ctags -R .
